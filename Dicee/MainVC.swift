474f2fa5e1c7304d613617faea77470fd00b5f25
Tiago Do Couto <cs.couto@outlook.com>
2017-08-29 16:17:30 -0300

    runnning

---
+++ Dicee/MainVC.swift
@@ -0,0 +1,37 @@
+//
+//  MainVC.swift
+//  Dicee
+//
+//  Created by Tiago Do Couto on 29/08/17.
+//  Copyright © 2017 Tiago Do Couto. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    //outlets
+    @IBOutlet weak var imgDice1: UIImageView!
+    @IBOutlet weak var imgDice2: UIImageView!
+    
+    //vars
+    var indexDice1 = 0
+    var indexDice2 = 0
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
+    //lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //actions
+    @IBAction func rollDices(_ sender: Any) {
+        indexDice1 = Int(arc4random_uniform(6))
+        indexDice2 = Int(arc4random_uniform(6))
+        
+        imgDice1.image =  UIImage(named: diceArray[indexDice1])
+        imgDice2.image =  UIImage(named: diceArray[indexDice2])
+    }
+
+}
+
